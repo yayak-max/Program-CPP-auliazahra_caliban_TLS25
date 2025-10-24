ac3cbed3b2d464a2b2213891e39088928dbb1022
yayak-max <auliazahrawahyudiapratiwi@mail.ugm.ac.id>
2025-10-24 22:58:32 +0700

    Add files via upload

---
+++ coding.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++){
+        std:: cout << i << '\n';
+    }
+
+    std::cout << "Merah Kuning\n";
+
+    return 0;
+}
